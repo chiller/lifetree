2d5a0285efd3184cc4b929213e5f9bc682674684
Galaczi Endre <galacziendre@gmail.com>
2015-01-10 18:54:51 +0100

    timelogadmin

--- xp/admin.py
+++ xp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from xp.models import TimeLog, Activity, BadgeType
 
+class TimeLogAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date', 'activity', 'notes', 'minutes']
+
 admin.site.register(Activity)
-admin.site.register(TimeLog)
+admin.site.register(TimeLog, TimeLogAdmin)
 admin.site.register(BadgeType)
